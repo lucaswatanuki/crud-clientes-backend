c5a491ca91c5afbbe646bd577f2a07ef0a98d54c
lucaswatanuki <lucaswatanuki@gmail.com>
2020-12-05 02:49:50 -0300

    ajuste login

--- login.php
+++ login.php
@@ -7,24 +7,23 @@ $request = json_decode($postdata);
 
 if (isset($postdata) && !empty($postdata)) {
     if (!empty($con)) {
-        $username = mysqli_real_escape_string($con, trim($request->username));
+        $username = mysqli_real_escape_string($con, $request->username);
     }
 
     $sql = "SELECT * FROM usuario where username='$username'";
 
     if ($result = mysqli_query($con, $sql)) {
-        $response = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            if (password_verify($request->password, $row['password'])) {
-                http_response_code(201);
-                $response = [
-                    "id" => $row['id'],
-                    "username" => $row['username']
-                ];
-                echo json_encode($response);
-            } else http_response_code(401);
+        http_response_code(200);
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($request->password, $row['password'])) {
+            $response = [
+                "username" => $row['username']
+            ];
+            http_response_code(200);
+            echo json_encode($response);
+        } else {
+            http_response_code(401);
         }
-        http_response_code(401);
     } else {
         http_response_code(404);
     }
